841b961e82c2d9de9210f5e6dcc28dc58b185e02
Prabhav Thali <Prabhav.Thali1@ibm.com>
2020-07-01 22:47:12 +0530

    latest version for s390x

--- dockerfiles/keycloak/build.sh
+++ dockerfiles/keycloak/build.sh
@@ -16,11 +16,11 @@ init --name:keycloak "$@"
 VERSION=6.0.1
 ARCH="$(uname -m)"
 if [ "${ARCH}" == "s390x" ]; then
-    VERSION=7.0.1
+    VERSION=10.0.2
     git clone https://github.com/keycloak/keycloak-containers.git
     cd keycloak-containers && git checkout tags/${VERSION}
     cd server && docker build --build-arg GIT_REPO=keycloak/keycloak --build-arg GIT_BRANCH=${VERSION} -t jboss/keycloak:${VERSION} .
-    cd ../..
+    cd "${base_dir}"
 fi
 
 build
